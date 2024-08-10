156ce38c4098ecb8776cb8e8bd85f3b4206ff810
shubhswain45 <shubhkantswain@gmail.com>
2024-08-10 20:46:20 +0530

    another changes

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
 
-import { connectDB } from "./db/connectDB.js";
+import { connetDB } from "./db/connectDB.js";
 
 import authRoutes from "./routes/auth.route.js";
 
